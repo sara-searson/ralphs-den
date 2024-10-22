b49f5946dc52687d1b87eceb8c152f8369f13072
sara-searson <sara.r.searson@gmail.com>
2024-10-21 21:58:34 -0400

    Started front end

---
+++ client/script.js
@@ -0,0 +1,6 @@
+const signUp = document.querySelector('#nav1')
+const library = document.querySelector('#nav2')
+const aboutUs = document.querySelector('#nav3')
+const gamePage = document.querySelector('#nav4')
+const sideBar = document.querySelector('#side-bar')
+
