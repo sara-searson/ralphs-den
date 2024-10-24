013156c001e657bef8dabb74a71bd39d1b8104d5
sara-searson <sara.r.searson@gmail.com>
2024-10-23 23:53:40 -0400

    Finished front page and worked on library page

---
+++ client/library.js
@@ -0,0 +1,90 @@
+const hash = window.location.hash.substring(1); // Remove the '#' character
+const params = new URLSearchParams(hash);
+let user = params.get('libid');
+
+const signUp = document.querySelector('#nav1')
+const home = document.querySelector('#nav2')
+const aboutUs = document.querySelector('#nav3')
+const gamePage = document.querySelector('#nav4')
+
+signUp.classList.add('hidden')
+
+const gameContainers = document.querySelectorAll('.game-container')
+const gameContainer1 = document.querySelector('.game-container1')
+const gameContainer2 = document.querySelector('.game-container2')
+const gameContainer3 = document.querySelector('.game-container3')
+const gameContainer4 = document.querySelector('.game-container4')
+const gameContainer5 = document.querySelector('.game-container5')
+const gameContainer6 = document.querySelector('.game-container6')
+const gameContainer7 = document.querySelector('.game-container7')
+const gameContainer8 = document.querySelector('.game-container8')
+
+const spot1 = document.querySelector('#spot1')
+const spot2 = document.querySelector('#spot2')
+const spot3 = document.querySelector('#spot3')
+const spot4 = document.querySelector('#spot4')
+const spot5 = document.querySelector('#spot5')
+const spot6 = document.querySelector('#spot6')
+const spot7 = document.querySelector('#spot7')
+const spot8 = document.querySelector('#spot8')
+const spots = [spot1, spot2, spot3, spot4, spot5, spot6, spot7, spot8]
+
+const img1 = document.querySelector('img1')
+const img2 = document.querySelector('img2')
+const img3 = document.querySelector('img3')
+const img4 = document.querySelector('img4')
+const img5 = document.querySelector('img5')
+const img6 = document.querySelector('img6')
+const img7 = document.querySelector('img7')
+const img8 = document.querySelector('img8')
+const imgs = [img1, img2, img3, img4, img5, img6, img7, img8]
+
+const loadLibrary = async () => {
+    try {
+        let response = await axios.get(`http://localhost:3001/entries/library/${user}`)
+        let entries = response.data
+        for (let i = 0; i < entries.length; i++) {
+            let currentGameId = entries[i].game
+            let currentGame = await axios.get(`http://localhost:3001/games/${currentGameId}`)
+            let currentGameData = currentGame.data
+            let currentSpot = spots[i]
+            console.log(currentSpot)
+            let gameContainer = document.createElement('div')
+                gameContainer.classList.add(`game-container`)
+                gameContainer.classList.add(`game-container${i + 1}`)
+            const gameTitle = document.createElement('h5')
+                gameTitle.classList.add('game-title')
+                gameTitle.innerText = currentGameData.title
+                console.log(currentGameData.title)
+                gameContainer.appendChild(gameTitle)
+            const gameRating = document.createElement('h6')
+                gameRating.classList.add('game-rating')
+                gameRating.innerText = currentGameData.esrb
+                gameContainer.appendChild(gameRating)
+            const gameGenres = document.createElement('h6')
+                gameGenres.classList.add('game-genres')
+                gameGenres.innerText = currentGameData.genre
+                gameContainer.appendChild(gameGenres)
+            const gameId = document.createElement('p')
+                gameId.classList.add('game-id')
+                gameId.innerText = currentGameData._id
+                gameContainer.appendChild(gameId)
+            currentSpot.appendChild(gameContainer)
+            // let currentImg = imgs[i]
+            // currentImg.setAttribute('src', '../assets/erd.jpeg')
+            console.log(currentGameData)
+        }
+    } catch (e) {
+        console.error(e)
+    }
+}
+
+loadLibrary()
+
+document.addEventListener('click', (event) => {
+    if (event.target && event.target.classList.contains('game-container')) {
+        gameId = document.querySelector('.game-id').innerText.get
+          console.log(`Game Clicked: ${gameId}`)
+        //   updateName()
+    }
+})
--- client/script.js
+++ client/script.js
@@ -1,10 +1,11 @@
 const signUp = document.querySelector('#nav1')
 const library = document.querySelector('#nav2')
-const aboutUs = document.querySelector('#nav3')
-const gamePage = document.querySelector('#nav4')
+const gamePage = document.querySelector('#nav3')
+const aboutUs = document.querySelector('#nav4')
 const sideBar = document.querySelector('#side-bar')
 
 let isLoggedIn = false
+library.classList.add('hidden')
 
 const addLoginSection = () => {
     const loginContainer = document.createElement('div')
@@ -39,6 +40,7 @@ const addUpdateInfo = (currentUser) => {
           updateContainer.classList.add('update-container')
     const greeting = document.createElement('h2')
           greeting.innerText = `Hello ${currentUser}`
+          greeting.classList.add('greeting')
           updateContainer.appendChild(greeting)
     const updateHeading = document.createElement('h3')
           updateHeading.innerText = 'Update Info'
@@ -46,38 +48,47 @@ const addUpdateInfo = (currentUser) => {
     const nameInput = document.createElement('input')
           nameInput.type = 'text'
           nameInput.placeholder = 'Name'
+          nameInput.classList.add('new-name')
           updateContainer.append(nameInput)
     const nameBtn = document.createElement('button')
           nameBtn.type = 'button'
+          nameBtn.classList.add('new-name-btn')
           nameBtn.innerText = 'Change Name'
           updateContainer.append(nameBtn)
     const usernameInput = document.createElement('input')
           usernameInput.type = 'text'
           usernameInput.placeholder = 'Username'
+          usernameInput.classList.add('new-username')
           updateContainer.append(usernameInput)
     const usernameBtn = document.createElement('button')
           usernameBtn.type = 'button'
           usernameBtn.innerText = 'Change Username'
+          usernameBtn.classList.add('new-username-btn')
           updateContainer.append(usernameBtn)
     const passwordInput = document.createElement('input')
           passwordInput.type = 'text'
           passwordInput.placeholder = 'New Password'
+          passwordInput.classList.add('new-password')
           updateContainer.append(passwordInput)
     const passwordBtn = document.createElement('button')
           passwordBtn.type = 'button'
           passwordBtn.innerText = 'Change Password'
+          passwordBtn.classList.add('new-password-btn')
           updateContainer.append(passwordBtn)
     const emailInput = document.createElement('input')
           emailInput.type = 'text'
-          emailInput.placeholder = 'Password'
+          emailInput.placeholder = 'Email'
+          emailInput.classList.add('new-email')
           updateContainer.append(emailInput)
     const emailBtn = document.createElement('button')
           emailBtn.type = 'button'
-          emailBtn.innerText = 'Change Password'
+          emailBtn.innerText = 'Change Email'
+          emailBtn.classList.add('new-email-btn')
           updateContainer.append(emailBtn)
     const currentPassword = document.createElement('input')
           currentPassword.type = 'password'
           currentPassword.placeholder = 'Current Password'
+          currentPassword.classList.add('current-password')
           updateContainer.append(currentPassword)
     sideBar.removeChild(document.querySelector('.login-section'))
     sideBar.appendChild(updateContainer)
@@ -96,6 +107,8 @@ const login = async () => {
                 document.querySelector('.login-section').classList.add('hidden')
                 addUpdateInfo(username)
                 console.log('logged in')
+                signUp.classList.add('hidden')
+                library.classList.remove('hidden')
                 return username
             } else {
                 const failedLogin = document.createElement('h3')
@@ -108,7 +121,175 @@ const login = async () => {
     }
 } //I looked at the Buy Bi Bye Cycle project from the group projects last week for help with this, so thanks to that group! 
 
+const updateName = async () => {
+      const newName = document.querySelector('.new-name').value
+      // console.log(newName)
+      const greeting = document.querySelector('.greeting').innerHTML
+      // console.log(greeting)
+      const username = greeting.replace('Hello ', "")
+      const currentPassword = document.querySelector('.current-password').value
+      try {
+            const response = await axios.get(`http://localhost:3001/users/name/${username}`)
+            const id = response.data._id
+            if (response.status === 200) {
+                  if (response.data.password === currentPassword) {
+                        const update = await axios.put(`http://localhost:3001/users/${id}`, { name: newName })
+                        if (update.status === 200) {
+                              alert('Name updated successfully!')
+                        } else {
+                              alert('Failed to update name')
+                        }
+                  } else {
+                        alert('Incorrect password')
+                  }
+            } else {
+                  alert('Error: Please try again')
+            }
+      } catch (e) {
+            console.log(`Error updating name:`, e)
+            alert('An errr occured while updating your name. Please try again')
+      }
+}
+
+const updateUsername = async () => {
+      const newUsername = document.querySelector('.new-username').value
+      const greeting = document.querySelector('.greeting').innerHTML
+      // console.log(greeting)
+      const username = greeting.replace('Hello ', "")
+      const currentPassword = document.querySelector('.current-password').value
+      try {
+            const response = await axios.get(`http://localhost:3001/users/name/${username}`)
+            const id = response.data._id
+            if (response.status === 200) {
+                  if (response.data.password === currentPassword) {
+                        const update = await axios.put(`http://localhost:3001/users/${id}`, { username: newUsername })
+                        if (update.status === 200) {
+                              alert('Username updated successfully!')
+                        } else {
+                              alert('Failed to update username')
+                        }
+                  } else {
+                        alert('Incorrect password')
+                  }
+            } else {
+                  alert('Error: Please try again')
+            }
+      } catch (e) {
+            console.log(`Error updating name:`, e)
+            alert('An errr occured while updating your username. Please try again')
+      }
+}
+
+const updatePassword = async () => {
+      const newPassword = document.querySelector('.new-password').value
+      const greeting = document.querySelector('.greeting').innerHTML
+      // console.log(greeting)
+      const username = greeting.replace('Hello ', "")
+      const currentPassword = document.querySelector('.current-password').value
+      try {
+            const response = await axios.get(`http://localhost:3001/users/name/${username}`)
+            const id = response.data._id
+            if (response.status === 200) {
+                  if (response.data.password === currentPassword) {
+                        const update = await axios.put(`http://localhost:3001/users/${id}`, { password: newPassword })
+                        if (update.status === 200) {
+                              alert('Password updated successfully!')
+                        } else {
+                              alert('Failed to update password')
+                        }
+                  } else {
+                        alert('Incorrect password')
+                  }
+            } else {
+                  alert('Error: Please try again')
+            }
+      } catch (e) {
+            console.log(`Error updating name:`, e)
+            alert('An errr occured while updating your password. Please try again')
+      }
+}
+
+const updateEmail = async () => {
+      const newEmail = document.querySelector('.new-email').value
+      const greeting = document.querySelector('.greeting').innerHTML
+      // console.log(greeting)
+      const username = greeting.replace('Hello ', "")
+      const currentPassword = document.querySelector('.current-password').value
+      try {
+            const response = await axios.get(`http://localhost:3001/users/name/${username}`)
+            const id = response.data._id
+            if (response.status === 200) {
+                  if (response.data.password === currentPassword) {
+                        const update = await axios.put(`http://localhost:3001/users/${id}`, { email: newEmail })
+                        if (update.status === 200) {
+                              alert('Email updated successfully!')
+                        } else {
+                              alert('Failed to update Email')
+                        }
+                  } else {
+                        alert('Incorrect password')
+                  }
+            } else {
+                  alert('Error: Please try again')
+            }
+      } catch (e) {
+            console.log(`Error updating name:`, e)
+            alert('An errr occured while updating your email. Please try again')
+      }
+}
+
+
+
+
+
 document.querySelector('.login-button').addEventListener('click', () => {
     login()
+    let isLoggedIn = true
+    return isLoggedIn
 })
 
+document.addEventListener('click', (event) => {
+      if (event.target && event.target.classList.contains('new-name-btn')) {
+            console.log('new name btn pressed')
+            updateName()
+      }
+})
+
+document.addEventListener('click', (event) => {
+      if (event.target && event.target.classList.contains('new-username-btn')) {
+            console.log('new username btn pressed')
+            updateUsername()
+      }
+})
+
+document.addEventListener('click', (event) => {
+      if (event.target && event.target.classList.contains('new-password-btn')) {
+            console.log('new password btn pressed')
+            updatePassword()
+      }
+})
+
+document.addEventListener('click', (event) => {
+      if (event.target && event.target.classList.contains('new-email-btn')) {
+            console.log('new email btn pressed')
+            updateEmail()
+      }
+})
+
+library.addEventListener('click', async () => {
+      const greeting = document.querySelector('.greeting').innerHTML
+      // console.log(greeting)
+      const username = greeting.replace('Hello ', "")
+      const currentPassword = document.querySelector('.current-password').value
+      const response = await axios.get(`http://localhost:3001/users/name/${username}`)
+      const id = response.data._id
+      window.location.href = `http://127.0.0.1:5500/client/library.html#${id}`
+})
+
+aboutUs.addEventListener('click', async () => {
+      window.location.href = 'http://127.0.0.1:5500/client/about.html'
+})
+
+gamePage.addEventListener('click', () => {
+      window.location.href = 'http://127.0.0.1:5500/client/game-info.html'
+})
